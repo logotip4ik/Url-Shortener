c12afa3429e34806e5f5a909c69e734d24cbae81
logotip4ik <bogdankostyuk12@gmail.com>
2020-10-30 20:11:14 +0200

    UPDATE: Started work on 'Finding links'

--- src/components/FindLink.vue
+++ src/components/FindLink.vue
@@ -1,9 +1,30 @@
 <template>
   <h1>Find Link By Its Slug</h1>
+  <div v-if="links.lenght >= 1">
+    <h2>Your Links</h2>
+    <div v-for="link in links" :key="link.name">
+      <pre>link</pre>
+    </div>
+  </div>
 </template>
 
 <script>
+import { inject, ref } from 'vue';
+
 export default {
   name: 'FindLink',
+  setup() {
+    const db = inject('db');
+    const links = ref([]);
+
+    async function getAllLinks() {
+      links.value = await db.value.getAll('links');
+    }
+    getAllLinks();
+
+    return {
+      links,
+    };
+  },
 };
 </script>
